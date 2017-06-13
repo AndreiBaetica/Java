46316eda1d3ee712db24eea69153f2432aaa7400
AndreiBaetica <andrei.baetic@gmail.com>
2017-06-12 23:33:16 -0400

    Added module 1

---
+++ Module 1/Application.java
@@ -0,0 +1,19 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("I have no idea what I'm doing.");
+		int nmbr;
+		short nmbr;
+		long nmbr;
+		double nmbr;
+		float nmbr;
+		nmbr=27;
+		char chr='y';
+		boolean bo=true;
+		byte byt=123;
+		
+		System.out.println(nmbr);
+	}
+
+}
