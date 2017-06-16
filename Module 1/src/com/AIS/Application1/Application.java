0d7d2520cf487a5bef30b86bebbc8dde3f0ebf68
Andrei Baetica <andrei.baetic@gmail.com>
2017-06-15 23:35:22 -0400

    Added more scanner code.

--- Module 1/src/com/AIS/Application1/Application.java
+++ Module 1/src/com/AIS/Application1/Application.java
@@ -5,57 +5,63 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("I have no idea what I'm doing.");
-		int nmbr;
-		short shortNmbr;
-		long longNmbr;
-		double doubleNmbr;
-		float floatNmbr;
-		nmbr=1;
-		char chr='y';
-		boolean bo=true;
-		byte byt=123;
-		boolean runThis=true; //global scope
-		
-		System.out.println(byt);
-		
-		String txt="<text goes here>";
-		while( nmbr==1) {
-			System.out.println("<Loop contents go here.>");
-			nmbr=27;
-//			boolean runThis=true; //variable with local scope
+		/*
+		 * System.out.println("I have no idea what I'm doing."); int nmbr; short
+		 * shortNmbr; long longNmbr; double doubleNmbr; float floatNmbr; nmbr=1;
+		 * char chr='y'; boolean bo=true; byte byt=123; boolean runThis=true;
+		 * //global scope
+		 * 
+		 * System.out.println(byt);
+		 * 
+		 * String txt="<text goes here>"; while( nmbr==1) {
+		 * System.out.println("<Loop contents go here.>"); nmbr=27; // boolean
+		 * runThis=true; //variable with local scope }
+		 * 
+		 * 
+		 * System.out.println(); System.out.println("Spacing:"); if (runThis) {
+		 * System.out.println("It ran!"); } else {
+		 * System.out.println("It didn't run!"); } System.out.println();
+		 * System.out.println();
+		 * 
+		 * for (int i=0; i<5; i++) { System.out.printf("The value of i is %d",
+		 * i); }
+		 * 
+		 * if( nmbr>20 ) { System.out.println("true"); }
+		 * 
+		 * else if( nmbr>20 ) { System.out.println("false"); }
+		 * 
+		 * System.out.println("test");
+		 * 
+		 * Scanner input = new Scanner(System.in);
+		 * System.out.println("Enter a line of text: "); String line =
+		 * input.nextLine(); System.out.println("You entered: "+line);
+		 * 
+		 * 
+		 * Scanner scanner = new Scanner(System.in);
+		 * System.out.println("enter a number: "); int value =
+		 * scanner.nextInt(); while(value != 5) {
+		 * System.out.println("Enter a number: "); value = scanner.nextInt(); }
+		 * 
+		 * Scanner scanner = new Scanner(System.in); int value = 0; do {
+		 * System.out.println("Enter a number: "); value = scanner.nextInt(); }
+		 * while(value!=5);
+		 * 
+		 * System.out.println("got 5.");
+		 */
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a command: ");
+		String text = input.nextLine();
+		switch (text) {
+		case "start":
+			System.out.println("Machine started.");
+			break;
+		case "stop":
+			System.out.println("Machine stopped.");
+			break;
+		default:
+			System.out.println("Command not recognized");
 		}
 
-		
-		System.out.println();
-		System.out.println("Spacing:");
-		if (runThis) {
-			System.out.println("It ran!");
-		} else {
-			System.out.println("It didn't run!");
-		}
-		System.out.println();
-		System.out.println();
-		
-		for (int i=0; i<5; i++) {
-			System.out.printf("The value of i is %d", i);
-		}
-		
-		if( nmbr>20 ) {
-			System.out.println("true");
-		}
-		
-		else if( nmbr>20 ) {
-			System.out.println("false");
-		}
-		
-		System.out.println("test");
-		
-		Scanner input; new Scanner(System.in);
-		System.out.println("Enter a line of text: ");
-		String line = input.nextLine();
-		System.out.println("You entered: "+line);
-		
 	}
 
 }
