c26a64394fc83f9b0d07ea09fb16d95f5e32ec82
Andrei Baetica <andrei.baetic@gmail.com>
2017-07-08 23:45:21 -0400

    finished module 2

--- Module 1/src/collections/App.java
+++ Module 1/src/collections/App.java
@@ -1,10 +1,36 @@
 package collections;
 
-public class App {
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class App {
+	public static String[] vehicles = {
+			"ambulance", "helicopter","lifeboat"
+	};
+	public static String[][] drivers = {
+			{"Fred","Sue","Pete"},
+			{"Sue","Dick","Bob","Fred"},
+			{"Pete","Mary","Bob"},	
+	};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<String, Set<String>> personnel = new HashMap<>();
+		for(int i=0; i<vehicles.length; i++){
+			String vehicle = vehicles[i];
+			String[] driverList = drivers[i];
+			Set<String> driverSet = new LinkedHashSet<String>();
+			for(String driver: driverList){
+				driverSet.add(driver);
+			}
+			personnel.put(vehicle, driverSet);
+		}
+		Set<String> driverList = personnel.get("helicopter");
+		for(String driver: driverList){
+			System.out.println(driver);
+		}
+		
 	}
 
 }
