c9f8c8610292188a17e2afc1dc9a6c6165389de0
Andrei Baetica <andrei.baetic@gmail.com>
2017-07-10 23:55:45 -0400

    Finished lambda expressions, started lists project

--- Module 1/src/lambda/App.java
+++ Module 1/src/lambda/App.java
@@ -1,13 +1,34 @@
 package lambda;
+interface Executable{
+	int execute();
+}
+
 class Runner {
-	public void run(){
-		
+	public void run(Executable e){
+		System.out.println("Executing code block...");
+		int value = e.execute();
+		System.out.println("Return value: "+value);
 	}
 }
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Runner runner = new Runner();
+		runner.run((new Executable(){
+			public int execute(){
+				System.out.println("Hello there");
+				return 7;
+			}
+		}));
+		
+		System.out.println("===================");
+		
+		runner.run(() -> {
+			System.out.println("Lambda expression code");
+			System.out.println("Hello there");
+			return 8;
+		});
 
 	}
 
---
+++ Module 2/src/lists/App.java
@@ -0,0 +1,13 @@
+package lists;
+
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List toDo = new List();
+		
+	}
+
+}
