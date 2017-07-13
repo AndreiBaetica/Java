cad4aa7f15287f3d6bb38cd01913592d03886ead
Andrei Baetica <andrei.baetic@gmail.com>
2017-07-12 23:20:10 -0400

    finished lists basic functionality

--- Module 2/src/lists/App.java
+++ Module 2/src/lists/App.java
@@ -4,30 +4,68 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+class Lists {
+	public static List<String> toDo = new LinkedList<String>();
+	public static List<String> completed = new LinkedList<String>();
+
+	public static void add(List l, String s) {
+		l.add(s);
+	}
+
+	public static void removeToDo(String s) {
+		toDo.remove(s);
+	}
+	public static void clear(List l) {
+		l.clear();
+	}
+}
+
 public class App {
+	// TODO: add are you sure check to option 4.
 
 	public static void main(String[] args) {
-		
-		
-		
-		List<String> toDo = new LinkedList<String>();
-		List<String> completed = new LinkedList<String>();
+
+		System.out.println("\n To Do List: " + Lists.toDo + "\n \n Completed tasks: " + Lists.completed + "\n \n");
 
 		Scanner reader = new Scanner(System.in);
-		System.out.println("Available commands: \n1. Add task \n2. Remove task \n3. Remove completed task \n4. View to-do list \n5. View completed tasks \n");
+		System.out.println("Available commands: \n1. Add task \n2. Remove task \n3. Complete task \n4. Clear all");
 		int n = reader.nextInt();
-		reader.close();
-		if (n < 1 || n > 5) {
+
+		if (n < 1 || n > 4) {
 			main(args);
 		}
 		if (n == 1) {
 			Scanner reader1 = new Scanner(System.in);
 			System.out.println("Enter a task to add: ");
 			String task = reader1.nextLine();
-			reader1.close();
-			toDo.add(task);
+			Lists.add(Lists.toDo, task);
 			main(args);
 		}
+		if (n == 2) {
+			Scanner reader2 = new Scanner(System.in);
+			System.out.println("Enter a task to remove: ");
+			String task = reader2.nextLine();
+			Lists.removeToDo(task);
+			main(args);
+		}
+		if (n == 3) {
+			Scanner reader3 = new Scanner(System.in);
+			System.out.println("Enter the completed task: ");
+			String task = reader3.nextLine();
+			if (Lists.toDo.contains(task)) {
+				Lists.add(Lists.completed, task);
+				Lists.removeToDo(task);
+			} else {
+				System.out.println("No such element in To Do list.");
+			}
+			main(args);
+		}
+		if (n == 4) {
 
+			Lists.toDo.clear();
+			Lists.completed.clear();
+			main(args);
+		}
+		reader.close();
 	}
 }
