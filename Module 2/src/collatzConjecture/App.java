31aa4425291b4473d37dda3c1c073880f0c9b7e2
Andrei Baetica <andrei.baetic@gmail.com>
2017-08-12 13:20:02 -0400

    Finished Collatz Conjencture

--- Module 2/src/collatzConjecture/App.java
+++ Module 2/src/collatzConjecture/App.java
@@ -1,19 +1,21 @@
 package collatzConjecture;
 
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// TODO: create recursion
 		int mn = 2;
 		int mnd = mn;
-		while (mnd!=1){
-			if (mnd%2==0){
-				mnd = mnd/2;
+		while (mn < 1000000000) {
+			while (mnd != 1) {
+				if (mnd % 2 == 0) {
+					mnd = mnd / 2;
+				} else {
+					mnd = (3 * mnd + 1);
+				}
+				System.out.println(mn + " does not slove the conjencture.");
+				mn = mn + 1;
+				mnd = mn;
 			}
-			else{
-				mnd = (3*mnd+1);
-			}
-		mn = mn+1;
 		}
-		
 	}
 }
